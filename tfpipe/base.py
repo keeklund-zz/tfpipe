94dd6847fd45ed4dd70c9b6a2871a4097ad235e6
Karl Eklund <eklundke@gmail.com>
2013-05-02 11:10:43 -0400

    reorg

--- tfpipe/base.py
+++ tfpipe/base.py
@@ -1,4 +1,10 @@
 """Base.py holds common functionality for future classes.
 
-
 """
+
+class CommandLine():
+    def __init__(self):
+        pass
+
+
+
---
+++ tfpipe/modules/galaxy/convert.py
@@ -0,0 +1,8 @@
+""" """
+from tfpipe.base import CommandLine
+
+class FastqToFasta(CommandLine):
+    def __init__(self):
+        pass
+
+
