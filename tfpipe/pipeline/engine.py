e02b6b48ac520d74ce4a919dd36589608dddd992
Karl Eklund <eklundke@gmail.com>
2013-05-06 16:14:10 -0400

    setting up job dependency in WorkFlow

--- tfpipe/pipeline/engine.py
+++ tfpipe/pipeline/engine.py
@@ -1,15 +1,15 @@
 """Defines functionality for pipeline.
 
 """
+from tfpipe.utils import logger
 
 class WorkFlow(object):
     """
 
     """
-    bsub = "bsub "
     def __init__(self, job_list=[]):
         self.jobs = job_list
-        
+        logger.info("WorkFlow created")
 
     def add_job(self, newjob):
         """ """
@@ -18,9 +18,13 @@ class WorkFlow(object):
     # check order of jobs
     def show(self):
         for job in self.jobs:
-            print job
+            print self._create_submit_str(job)
     
-
+    def _create_submit_str(self, job):
+        bsub = "bsub -J %s " % job.name
+        tmp = "&&".join([d.name for d in job.dep])
+        bsub += "-w done(%s) " % tmp if job.dep else '' 
+        return bsub + str(job)
 # possible to add attributes to job from within workflow?
 # need ability to specify how dependency should work, whether 
 # it's done, exited, ended, etc.
