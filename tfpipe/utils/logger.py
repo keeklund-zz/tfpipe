717d1b159d6223631c0c70c270736a9460ab65dc
Karl Eklund <eklundke@gmail.com>
2013-05-06 14:32:19 -0400

    moved logger to appropriate locations

---
+++ tfpipe/utils/logger.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+from os import path
+import logging
+import logging.config
+
+conffile = path.join(path.dirname(path.abspath(__file__)), 'logconf.py')
+logging.config.fileConfig(conffile)
+logger = logging
+
