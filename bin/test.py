f53dc5c0373cbf7d20ed117759a9fdca8a12f16b
Karl Eklund <eklundke@gmail.com>
2013-05-16 12:30:52 -0400

    updated example

--- bin/test.py
+++ bin/test.py
@@ -14,35 +14,36 @@ job1 = FastqQualityFilter(name='kure_fastqqualityfilter',
                                 '-q': '20',
                                 '-p': '100',
                                 },)
+job1.redirect_output(out_dir + 'test.fastq')
 
 job2 = FastqToFasta(name='kure_fastq_to_fasta')
 job2.add_argument('-i', out_dir + 'test.fastq')
 job2.add_argument('-o', out_dir + 'test.fasta')
 job2.add_dependencies(done=[job1,])
-job2.redirect_output(out_dir + 'test.fastq')
 
 job_list = [job1, job2]
 
 for i in range(1, 60):
     gsnap_args = {'--terminal-threshold=10': '',
                   '-n': '1',
-                  '--genome-unk-mismatch=1':'',
+                  '--genome-unk-mismatch=1': '',
                   '--query-unk-mismatch=1': '',
                   '-D': '/proj/fureylab/karl/pipeline_files/gmapdb/hg19_femalle_all/',
                   '-d': 'hg19_female',
                   '-v': 'CD_FAIRE.snps.gsnap',
-                  '--trim-mistmatch-score=0': '',
+                  '--trim-mismatch-score=0': '',
                   '-m': '1',
                   '-A': 'sam', 
                   '-i': '3',
                   '-k': '15',
                   '--basesize=12': '',
                   '--sampling=1': '',
-                  '-q': '%s/60' % i,}
-    exec("job%s = Gsnap(name='kure_gsnap%s', args=gsnap_args)" % (i, i))
-    exec("job%s.add_dependencies(done=[job1,])" % i)
-    exec("job%s.redirect_output('somewhere')" % i)
-    job_list.append(eval("job%s" % i))
+                  '-q': '%s/60' % i,
+                  '%s/test.fastq' % out_dir : ''}
+    exec("gsnap_job%s = Gsnap(name='kure_gsnap%s', args=gsnap_args)" % (i, i))
+    exec("gsnap_job%s.add_dependencies(done=[job1,])" % i)
+    exec("gsnap_job%s.redirect_output('somewhere')" % i)
+    job_list.append(eval("gsnap_job%s" % i))
          
 wf = WorkFlow(job_list)
 wf.show()
