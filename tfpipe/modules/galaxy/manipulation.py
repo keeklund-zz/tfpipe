fce772432976d7553a3134f73a987bc62360dd75
Karl Eklund <eklundke@gmail.com>
2013-05-22 10:30:42 -0400

    override __str__ base class method to allow for piping a job allowing for multiple adapter sequences

--- tfpipe/modules/galaxy/manipulation.py
+++ tfpipe/modules/galaxy/manipulation.py
@@ -26,6 +26,26 @@ class FastxClipper(Galaxy):
     """
     _cmd = 'fastx_clipper'
 
+    def __str__(self):
+        """Represent object as string.
+
+        Override base class to pipe multiple adapter sequences.
+
+        """
+        job_str = ''
+        for adpt in self.adapters:
+            tmp = " ".join((self.cmd, self._parse_args(), '-a', adpt))
+            job_str = " | ".join((job_str, tmp))
+        redirect = " %s %s " % (">", self.redirect) if self.redirect else ''
+        return " ".join((job_str, redirect))
+
+    def add_adapter_file(self, apt_file):
+        """Method allows user to submit multiple adapter sequences at once. 
+
+        """
+        with open(apt_file, 'r') as f:
+            self.adapters = f.read().split()
+
 
 class FastqQualityFilter(Galaxy):
     """Filters reads below specified quality.
