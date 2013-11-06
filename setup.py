e36d0a20149c8d302a1893d5efed477a431df03c
Karl Eklund <eklundke@gmail.com>
2013-11-06 16:13:09 -0500

    updated version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='tfpipe',
-    version='0.1.1',
+    version='0.1.2',
     author='Karl Eklund',
     author_email='keklund@email.unc.edu',
     packages=['tfpipe', 
