2da314d7690edea120a58b4ba330bc659e1a7217
Karl Eklund <eklundke@gmail.com>
2013-11-18 13:18:54 -0500

    0.1.3 added DFilter

--- tfpipe/__init__.py
+++ tfpipe/__init__.py
@@ -1,12 +1,13 @@
 """
 
 """
-import tfpipe.modules.galaxy
-import tfpipe.modules.gmap
 import tfpipe.modules.cli
-import tfpipe.modules.picard
-import tfpipe.modules.samtools
-import tfpipe.modules.bedtools
+import tfpipe.modules.gmap
 import tfpipe.modules.mach
 import tfpipe.modules.plink
+import tfpipe.modules.galaxy
+import tfpipe.modules.picard
+import tfpipe.modules.dfilter
+import tfpipe.modules.bedtools
+import tfpipe.modules.samtools
 import tfpipe.pipeline
