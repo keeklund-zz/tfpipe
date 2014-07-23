d9f15149b2918719517dd7c2f900be14263f3631
Karl Eklund <eklundke@gmail.com>
2014-07-23 14:20:54 -0400

    initial addition

--- tfpipe/modules/qiime/tools.py
+++ tfpipe/modules/qiime/tools.py
@@ -5,6 +5,18 @@ class Qiime(Job):
     """
 
     """
-    _module = 'qiime/1.5.0'
+    _module = 'qiime/1.8.0'
 
 
+class JoinPairedEnds(Qiime):
+    """
+
+    """
+    _cmd = 'join_paired_ends.py'
+
+
+class ConvertFastaQualFastq(Qiime):
+    """
+
+    """
+    _cmd = 'convert_fastaqual_fastq.py'
