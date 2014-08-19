0ab49be11d1fc1ea4c6dff31b274c8b24c7f6941
Karl Eklund <eklundke@gmail.com>
2014-08-19 15:32:02 -0400

    fixed helper function

--- tfpipe/utils/helper.py
+++ tfpipe/utils/helper.py
@@ -1,15 +1,18 @@
-"""Simple, but common functions typically used in pipelines.
+"""Simple, but common functions typically used in pipelines.                   
 
 """
 from mimetypes import guess_type
+from os.path import dirname
+from os.path import basename as bname
 from os.path import join as path_join
 
 def get_file_location_info(some_file):
-    """Return path, filename, basename tuple.
-
+    """Return path, filename, basename tuple.                                      
+                                                                                   
     """
     try:
-        path,filename = some_file.split('/')
+        path = dirname(some_file)
+        filename = bname(some_file)
     except ValueError:
         path, filename = ('.', some_file)
     basename = '.'.join(filename.split('.')[:-1])
@@ -18,8 +21,13 @@ def get_file_location_info(some_file):
     return (path, filename, basename)
 
 def build_output(out_dir, prepend, basename, ext):
-    """Return simple path join of required input.
-
+    """Return simple path join of required input.                                  
+                                                                                   
     """
     return path_join(out_dir, ''.join([prepend, basename, ext]))
+
+
+
+
+
 
