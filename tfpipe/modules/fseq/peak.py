39f5f61501012272e2e577ce114b0d22818ba0c3
Paul A Cotney <paulcotn@email.unc.edu>
2017-03-20 12:08:26 -0400

    Removed option from the slurm command for fseq since it should be handled by the actual pipeline script and not by the pipeline framework.

--- tfpipe/modules/fseq/peak.py
+++ tfpipe/modules/fseq/peak.py
@@ -16,7 +16,7 @@ class FseqJava(Job):
     """
 
     """
-    _cmd = 'java -Xmx8000M -cp /proj/fureylab/code_repository/paulcotn/fseq/commons-cli-1.1.jar:/proj/fureylab/code_repository/paulcotn/fseq/fseq.jar edu.duke.igsp.gkde.Main -v  -o ./tmp/fseq -f 0 -of npf -b /proj/fureylab/genomes/human/hg19_reference/fseq/bff_50'
+    _cmd = 'java -Xmx8000M -cp /proj/fureylab/code_repository/paulcotn/fseq/commons-cli-1.1.jar:/proj/fureylab/code_repository/paulcotn/fseq/fseq.jar edu.duke.igsp.gkde.Main '
 
     # This commmand is SLURM only
     _memory_req_slurm = "100G"
